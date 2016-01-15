5a86fef38bc57d306366353744ab25db625d9a38
Aleksandar Trposki <aleksandartrposki@gmail.com>
2016-01-15 21:11:16 +0100

    Added all

---
+++ example project/Assets/Scripts/EventsExposing/Common/BaseEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.EventsExposing.Common
+{
+    class BaseEvent<EventOrigin>
+    {
+        public EventOrigin Origin { get; set; }
+    }
+}
